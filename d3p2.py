95f8d0329a0e623f978b5ce891055aa1c143c7b0
Michael Sheeran <misterhaderach@gmail.com>
2020-12-05 15:22:58 -0600

    working solution for d3p2!

---
+++ d3p2.py
@@ -0,0 +1,44 @@
+import pdb
+from functools import reduce
+
+class Skiier:
+    def __init__(self, x_slope=3, y_slope=1):
+        self.x = 0
+        self.y = 0
+        self.x_slope = x_slope
+        self.y_slope = y_slope
+        
+        self.trees_encountered = 0
+        self.course_complete = False
+    def __repr__(self):
+        return str(self.trees_encountered)
+    
+    def ski(self, ski_map: list) -> None:
+        self.x = (self.x + self.x_slope) % (len(ski_map[0]))
+        self.y += self.y_slope
+        
+        if self.y >= len(ski_map) - 1:
+            self.course_complete = True
+            return None
+        else:
+            if ski_map[self.y][self.x] == '#':
+                self.trees_encountered += 1
+        # print(f"({self.x},{self.y}): {ski_map[self.y][self.x]}")
+
+with open("5_input.txt") as input_file:
+    
+    map_array = input_file.read().strip().split('\n')
+
+    skiiers = [
+        Skiier(x_slope=slope[0], y_slope=slope[1])
+        for slope
+        in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    ]
+
+    for skiier in skiiers:
+        while not skiier.course_complete:
+            skiier.ski(map_array)
+    
+    answer = reduce(lambda x, y: x*y, [skiier.trees_encountered for skiier in skiiers])
+    print(answer)
+    
